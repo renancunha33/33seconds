03661c1e37e2a57affbcf9c4d9bcb6350ddd06f1
Renan Cunha <renan.cunha33@gmail.com>
2015-11-08 17:57:20 -0200

    INtegrado com a playstore

--- app/src/main/java/com/renan/seconds33/MainActivity.java
+++ app/src/main/java/com/renan/seconds33/MainActivity.java
@@ -2,24 +2,31 @@ package com.renan.seconds33;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.*;
+import com.google.android.gms.plus.Plus;
+import com.google.example.games.basegameutils.BaseGameUtils;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.games.Games;
 import com.renan.seconds33.DAO.PontuacaoDao;
 import com.renan.seconds33.model.Pontuacao;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     ImageButton b1;
     ImageButton b2;
     ImageButton titulo;
@@ -28,9 +35,9 @@ public class MainActivity extends AppCompatActivity {
     boolean inicio, ch = false;
     int click = 0;
     public int teste = 0;
-    AlertDialog builder;
     PontuacaoDao pontuacaoDao;
     Pontuacao model;
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +47,15 @@ public class MainActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //================================ GOOGLE API
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API).addScope(Plus.SCOPE_PLUS_LOGIN)
+                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
+                .build();
+        //mGoogleApiClient.connect();
 
         //===============================variaveis.
         b1 = (ImageButton) findViewById(R.id.bt_start_bt1);
@@ -109,15 +125,13 @@ public class MainActivity extends AppCompatActivity {
             public void onChronometerTick(Chronometer chronometer) {
                 teste++;
                 if (chtempo.getText().equals("00:33")) {
-
-                    // Toast.makeText(getBaseContext(), "SCORE :" + String.valueOf(click), Toast.LENGTH_LONG).show();
-
                     //================================ if score > score salvo no banco, atualizar aqui
                     if (model.getScore() < click) {
 
                         model.set_id(1);
                         model.setScore(click);
                         long resultado = pontuacaoDao.AtualizarPontuacao(model);
+                        Games.Leaderboards.submitScore(mGoogleApiClient, getString(R.string.leaderboard_best_score), click);
                         if (resultado != -1) {
                             //  Toast.makeText(getBaseContext(), "Salvo",
                             //          Toast.LENGTH_LONG).show();
@@ -138,11 +152,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void msg() {
+        if (click > 100) {
+            Games.Achievements.unlock(mGoogleApiClient, getString(R.string.achievement_100_clicks));
+        }
+        if (click > 250) {
+            Games.Achievements.unlock(mGoogleApiClient, getString(R.string.achievement_250_clicks));
+        }
+        if (click > 350) {
+            Games.Achievements.unlock(mGoogleApiClient, getString(R.string.achievement_350_clicks));
+        }
+        if (click == 1) {
+            Games.Achievements.unlock(mGoogleApiClient, getString(R.string.achievement_desito));
+        }
+        if (click == 33) {
+            Games.Achievements.unlock(mGoogleApiClient, getString(R.string.achievement_33));
+        }
         AlertDialog alertDialog = new AlertDialog.Builder(this).create();
         alertDialog.setTitle("RECORD!!");
         alertDialog.setMessage("Score: " + String.valueOf(click));
         alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
+
                 finish();
                 startActivity(getIntent());
             }
@@ -152,6 +182,21 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void msgSad() {
+        if (click > 100) {
+            Games.Achievements.unlock(mGoogleApiClient, getString(R.string.achievement_100_clicks));
+        }
+        if (click > 250) {
+            Games.Achievements.unlock(mGoogleApiClient, getString(R.string.achievement_250_clicks));
+        }
+        if (click > 350) {
+            Games.Achievements.unlock(mGoogleApiClient, getString(R.string.achievement_350_clicks));
+        }
+        if (click == 1) {
+            Games.Achievements.unlock(mGoogleApiClient, getString(R.string.achievement_desito));
+        }
+        if (click == 33) {
+            Games.Achievements.unlock(mGoogleApiClient, getString(R.string.achievement_33));
+        }
         AlertDialog alertDialog = new AlertDialog.Builder(this).create();
         alertDialog.setTitle("Maybe tomorrow");
         alertDialog.setMessage("Score: " + String.valueOf(click));
@@ -162,5 +207,82 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         alertDialog.show();
+    }
+
+    private static int RC_SIGN_IN = 9001;
+
+    private boolean mResolvingConnectionFailure = false;
+    private boolean mAutoStartSignInFlow = true;
+    private boolean mSignInClicked = false;
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // Attempt to reconnect
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (mResolvingConnectionFailure) {
+            // already resolving
+            return;
+        }
+
+        // if the sign-in button was clicked or if auto sign-in is enabled,
+        // launch the sign-in flow
+        if (mSignInClicked || mAutoStartSignInFlow) {
+            mAutoStartSignInFlow = false;
+            mSignInClicked = false;
+            mResolvingConnectionFailure = true;
+
+            // Attempt to resolve the connection failure using BaseGameUtils.
+            // The R.string.signin_other_error value should reference a generic
+            // error string in your strings.xml file, such as "There was
+            // an issue with sign-in, please try again later."
+            if (!BaseGameUtils.resolveConnectionFailure(this,
+                    mGoogleApiClient, connectionResult,
+                    RC_SIGN_IN, "outro erro")) {
+                mResolvingConnectionFailure = false;
+            }
+        }
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == RC_SIGN_IN) {
+            mSignInClicked = false;
+            mResolvingConnectionFailure = false;
+            if (resultCode == RESULT_OK) {
+                mGoogleApiClient.connect();
+            } else {
+                // Bring up an error dialog to alert the user that sign-in
+                // failed. The R.string.signin_failure should reference an error
+                // string in your strings.xml file that tells the user they
+                // could not be signed in, such as "Unable to sign in."
+                BaseGameUtils.showActivityResultError(this,
+                        requestCode, resultCode, R.string.sign_in_failed);
+            }
+        }
+    }
+
+    final String TAG = "TanC";
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart(): connecting");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStart(): not disconnect");
+        // mGoogleApiClient.disconnect();
     }
 }
